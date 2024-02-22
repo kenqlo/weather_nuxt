02d74cc783ff5469c5109a4d00242a9c4881f124
Ken Kurosaki <kenqlo@gmail.com>
2024-02-22 16:00:37 +0900

    fixed loading city data

--- cityDB.ts
+++ cityDB.ts
@@ -1,5 +1,38 @@
 import type { City } from "@/interfaces";
-import cityList from "@/city-list.json";
+// import cityList from "@/city-list.json";
+
+const cityList = [
+  {
+      "id": 1853909,
+      "name": "大阪",
+      "q": "Osaka"
+  },
+  {
+      "id": 1850144,
+      "name": "東京",
+      "q": "Tokyo"
+  },
+  {
+      "id": 1859171,
+      "name": "神戸",
+      "q": "Kobe"
+  },
+  {
+      "id": 1862627,
+      "name": "姫路",
+      "q": "Himeji"
+  },
+  {
+      "id": 1855431,
+      "name": "新潟",
+      "q": "Niigata"
+  },
+  {
+      "id": 2127436,
+      "name": "横浜",
+      "q": "Yokohama"
+  }
+];
 
 //都市情報をJSONファイルから取得してデータセットを作成
 export function createCityList(): Map<number, City> {
