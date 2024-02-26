4112897a2df6ec3bc7bcc948917c7b6c00f1ee20
Ken Kurosaki <kenqlo@gmail.com>
2024-02-26 17:36:47 +0900

    divided into several components and added codes using Provide/Inject

--- pages/WeatherInfo/[id].vue
+++ pages/WeatherInfo/[id].vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
-import type {City} from "@/interfaces";
+import type {City, WeatherData} from "@/interfaces";
 
 const route = useRoute();
-const cityList = useState<Map<number, City>>("cityList");
+// const cityList = inject("cityList") as Map<number, City>; // Provide/Inject ver.
+const cityList = useState<Map<number, City>>("cityList"); // useState ver.
 const selectedCity = computed(
 	(): City => {
 		const idNo = Number(route.params.id);
-		return cityList.value.get(idNo) as City;
+		// return cityList.get(idNo) as City; // Provide/Inject ver.
+		return cityList.value.get(idNo) as City; // useState ver.
 	}
 );
 const asyncData = useWeatherInfoFetcher(selectedCity.value);
-const weatherData = asyncData.data;
+const weatherData: Ref<WeatherData | null> = asyncData.data;
 const pending = asyncData.pending;
 </script>
 
 <template>
 	<p v-if="pending">データ取得中…</p>
 	<section v-else>
-		<h2>{{selectedCity.name}}の天気</h2>
-		<p style="display: flex; align-items: center;"><img v-bind:src="weatherData.icon" alt="" />{{weatherData.description}}</p>
-		<p>最高気温：{{ weatherData.tempMax }}℃　最低気温：{{ weatherData.tempMin }}℃</p>
+		<h2>
+			<CityLabel v-bind:cityName="selectedCity.name"><span style="font-size: 0.75em;">の現在の天気</span></CityLabel>
+		</h2>
+		<template v-if="weatherData">
+			<WeatherInfo v-bind:weatherData="weatherData" />
+			<TemperatureInfo v-bind:weatherData="weatherData" />
+		</template>
 	</section>
 	<p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import type { City } from "@/interfaces";
 
+// // Provide/Inject ver.
+// const cityList = inject("cityList") as Map<number, City>;
+
+// useState ver.
 //都市情報リストをステートから取得。
 const cityList = useState<Map<number, City>>("cityList");
+
+// Pinia ver.
 </script>
 
 <template>
@@ -11,7 +17,7 @@ const cityList = useState<Map<number, City>>("cityList");
     <ul>
       <li v-for="[id, city] in cityList" v-bind:key="id">
         <NuxtLink v-bind:to="{ name: 'WeatherInfo-id', params: { id: id } }">
-          {{ city.name }}の天気
+          <CityLabel v-bind:cityName="city.name"/>
         </NuxtLink>
       </li>
     </ul>
